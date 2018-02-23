05733703a553be8e4784989182eb20972fed6eba
Adnan Aslam <adnanaslam@Adnan-Aslam411.local>
2018-02-23 23:40:44 +0530

    angular map integration structure // minor change // structured format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AgmCoreModule } from 'angular2-google-maps/core';
-
-
 import { AppComponent } from './app.component';
-import { MapViewComponent } from './map-view/map-view.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapViewComponent
   ],
   imports: [
     BrowserModule
